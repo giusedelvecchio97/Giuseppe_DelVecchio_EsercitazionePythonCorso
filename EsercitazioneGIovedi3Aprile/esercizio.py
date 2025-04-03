c15924792af14498ed3f2fd1e3b708b9d93e634d
giusedelvecchio97 <40830446+giusedelvecchio97@users.noreply.github.com>
2025-04-03 17:54:35 +0200

    Add files via upload

---
+++ EsercitazioneGIovedi3Aprile/esercizio.py
@@ -0,0 +1,82 @@
+
+class Punto():
+    
+    
+    def _init_ (self,x,y):
+        self.x
+        self.y
+        
+    
+    def muovi(self,dx,dy):
+        self.x=dx
+        self.y=dy
+        
+    
+    def calcola_distanza(self,dx,dy):
+        
+        coord=[]
+        coord[0]=dx-self.x
+        coord[1]=dy-self.y
+        
+        
+        return coord
+    
+    
+dx=6
+dy=9
+P=Punto(6,9)
+
+t=10
+r=12
+
+P.muovi(2,5)
+lista= P.calcola_distanza(t,r)
+print(lista)
+
+
+
+
+
+
+class Piano():
+    
+    def _init_ (self,lista):
+        self.lista=lista
+        
+    
+    
+    def ricerca(self,Punto):
+        
+        for v in lista:
+            
+            if(Punto in lista):
+                
+                print("Punto presente")
+            else:
+                
+                lista.append(Punto)
+                print("oggetto salvato")
+        
+
+
+i=0
+for i in range(10):
+    
+   x=int(input("inserisci coordinate x"))
+   y=int(input("inserisci coordinate y"))
+   lista[i]=Punto(x,y) 
+    
+
+Cartesiano=Piano(lista)
+
+
+print("adesso ricerca un punto")
+x=int(input("inserisci coordinate x"))
+y=int(input("inserisci coordinate y"))
+P1=Punto(x,y)
+Cartesiano.ricerca(P1)
+                
+            
+                
+
+
---
+++ EsercitazioneGIovedi3Aprile/ristorante.py
@@ -0,0 +1,140 @@
+
+class Ristorante():
+    
+    menu=[]
+    aperto=False
+    
+    
+    def __init__(self,nome,cucina):
+        
+        
+        self.nome=nome
+        self.cucina=cucina
+       
+        
+        
+    
+    def descrivi_ristorante(self):
+        
+        print("IL RISTORANTE SI CHIAMA ",self.nome,"\n LA SUA CUCINA E' DI TIPO ",self.cucina,"IL MENU DA NOI SERVITO E' QUESTO : \n,",self.menu)
+        
+    
+    
+    def stato_apertura(self):
+        
+        print(self.aperto)
+    
+    
+    
+    
+    def apertura(self,risposta):
+        
+        if(risposta=="SI"):
+            
+            self.aperto=True
+            print("RISTORANTE APERTO")
+            
+    
+        else:
+            self.aperto=False 
+            print("RISTORANTE CHIUSO")
+            
+    
+    
+    def mostra_menu(self):
+        print("\n Menu aggiornato:")
+        for voce in self.menu:
+            print(f'{voce["piatto"]}: {voce["prezzo"]}€')
+        print("-" * 20)
+    
+    
+   
+    def rimuovi_piatto(self,nome_piatto):
+       
+      
+        for elemento in Ristorante.menu:
+            if elemento["piatto"] == nome_piatto:
+                self.menu.remove(elemento)
+       
+        print(f' "{nome_piatto}" rimosso dal menu.')
+        
+    
+    def aggiungi_piatto(self,nome_piatto, prezzo_piatto):
+         self.menu.append({"piatto": nome_piatto, "prezzo": prezzo_piatto})
+         
+    
+ 
+ 
+
+def aggiungi_piatto(ristorante):
+    
+    
+    nome_piatto=input("inserisci nome del piatto")
+    prezzo=int(input("inserisci prezzo"))
+    
+    ristorante.aggiungi_piatto(nome_piatto,prezzo)
+    
+
+def rimozione(ristorante):
+    
+    piatto=input("inserisci nome del piatto da rimuovere")
+    ristorante.rimuovi_piatto(piatto)
+    
+    
+def apertura(ristorante):  
+    
+    risposta=input("ciao vuoi aprire il ristorante?")
+    ristorante.apertura(risposta)
+
+def menu(ristorante):
+    
+    Ristorante1.mostra_menu()
+    
+    
+    
+     
+     
+ 
+    
+    
+    
+
+#PROGRAMMA
+
+
+c=True
+Ristorante1=Ristorante("Villa Borghese","Cucina Contemporanea")
+while(c):
+    
+    
+    scelta=int(input("CIAO, SELEZIONA LA TUA OPZIONE :\n 1.STAMPA DESCRIZIONE\n2.STATO APERTURA\n3APERTURA O CHIUSURA\n4.AGGIUNGI PIATTO\n4.RIMUOVI PIATTO\n5.STAMPA MENU"))
+    
+    match scelta:
+            
+            case 1:
+                Ristorante1.descrivi_ristorante
+            
+            case 2:
+                
+                Ristorante1.stato_apertura()
+            case 3:
+                Ristorante1.apertura(Ristorante1)
+                
+            case 4:
+                aggiungi_piatto(Ristorante1)
+            
+            case 5:
+                rimozione(Ristorante1)
+            
+            case 6: 
+                menu(Ristorante1)
+            
+            
+            case 7: 
+                print("ESCI DAL SISTEMA")
+                c=False
+                
+
+
+           
+        
